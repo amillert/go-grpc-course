7ec2739da8a81abf8f40bf27aa4922a26ab27991
amillert <devmood.business@gmail.com>
2022-03-13 19:08:18 +0100

    update generation script for sum service

--- generate.sh
+++ generate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# greet rpc api
 protoc --go_out=grpc --proto_path=grpc/proto grpc/proto/greet_msg.proto
 protoc --go-grpc_out=grpc --go-grpc_opt=require_unimplemented_servers=false --proto_path=grpc/proto grpc/proto/greet_svc.proto
+
+# sum rpc api
+protoc --go_out=grpc --proto_path=grpc/proto grpc/proto/sum_msg.proto
+protoc --go-grpc_out=grpc --go-grpc_opt=require_unimplemented_servers=false --proto_path=grpc/proto grpc/proto/sum_svc.proto
