e52a1a0175d7bfa32a5588ff4cca6f8357463f21
amillert <devmood.business@gmail.com>
2022-03-13 18:54:44 +0100

    update generation protoc script

--- greet/generate.sh
+++ greet/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 protoc --go_out=grpc --proto_path=grpc/proto grpc/proto/greet_msg.proto
-protoc --go-grpc_out=grpc --proto_path=grpc/proto grpc/proto/greet_svc.proto
+protoc --go-grpc_out=grpc --go-grpc_opt=require_unimplemented_servers=false --proto_path=grpc/proto grpc/proto/greet_svc.proto
